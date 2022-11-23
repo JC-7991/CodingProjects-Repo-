4fd9750c40a3e01e88756b1f676776d5472c6a75
JC <jctblock@gmail.com>
2022-11-22 21:55:25 -0500

    Checkpoint

--- Data Structures/MaxHeap.py
+++ Data Structures/MaxHeap.py
@@ -1,3 +1,10 @@
 class MaxHeap:
+
     def __init__(self, items = []):
+
         super().__init__()
+        self.heap = 0
+        
+        for item in items:
+            self.heap.append(item)
+            self.__floatUp(len(self.heap) - 1)
