079b320925d485164ee379dde94037dfbeb3d8c9
JC <jctblock@gmail.com>
2022-11-24 00:06:00 -0500

    Checkpoint

--- DailyCoding/Week 4/task28.py
+++ DailyCoding/Week 4/task28.py
@@ -38,5 +38,6 @@ def justify(words, z):
 if __name__ == '__main__':
 
 	print("\n".join(justify(["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"], 16)))
+	print("\n".join(justify(["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"], 14)))
 	print("\n".join(justify(["sonic", "heroes", "is", "the", "best", "game", "ever", "made"], 16)))
 	print("\n".join(justify(["sonic", "heroes", "is", "the", "best", "game", "ever", "made"], 20)))
