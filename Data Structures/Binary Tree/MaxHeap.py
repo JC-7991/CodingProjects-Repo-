dd65944fe63c4bb1631c7cb91dd8ecf6b0554e8f
JC <jctblock@gmail.com>
2022-11-25 12:05:07 -0500

    Checkpoint

--- Data Structures/Binary Tree/MaxHeap.py
+++ Data Structures/Binary Tree/MaxHeap.py
@@ -41,11 +41,15 @@ class MaxHeap:
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
     def __floatUp(self, index):
+
         parent = index // 2
+
         if index <= 1:
             return
+            
         elif self.heap[index] > self.heap[parent]:
             self.__swap(index, parent)
+            self.__floatUp(parent)
 
     def __bubbleDown(self, index):
         pass
