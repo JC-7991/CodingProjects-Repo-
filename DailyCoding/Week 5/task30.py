63f8e2cc1d0d146955c8c31ac10d6c12d1ce2e6a
JC <jctblock@gmail.com>
2022-11-25 12:02:38 -0500

    Checkpoint

--- DailyCoding/Week 5/task30.py
+++ DailyCoding/Week 5/task30.py
@@ -5,3 +5,28 @@
 # Compute how many units of water remain trapped on the map 
 # in O(N) time and O(1) space.
 
+def max_water(arr, x):
+  
+    res = 0
+  
+    for i in range(1, x - 1):
+ 
+        left = arr[i]
+
+        for j in range(i):
+            left = max(left, arr[j])
+
+        right = arr[i]
+  
+        for j in range(i + 1, x):
+            right = max(right, arr[j])
+
+        res = res + (min(left, right) - arr[i])
+  
+    return res
+  
+if __name__ == "__main__":
+  
+    arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    x = len(arr)
+    print(max_water(arr, x))
