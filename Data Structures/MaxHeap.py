9511d3e3b3983bd31a9274059ed60892ba3385ce
JC <jctblock@gmail.com>
2022-11-25 00:04:08 -0500

    Checkpoint

--- Data Structures/MaxHeap.py
+++ Data Structures/MaxHeap.py
@@ -1,4 +1,6 @@
-# A Max-Heap is a complete binary tree in which the value in each internal node is greater than or equal to the values in the children of that node.
+# A Max-Heap is a complete binary tree in which the value 
+# in each internal node is greater than or equal to the 
+# values in the children of that node.
 
 class MaxHeap:
 
