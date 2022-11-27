d1ac4787a1a72574199de087114106b1684ce5d5
JC <jctblock@gmail.com>
2022-11-27 00:01:37 -0500

    Checkpoint

--- Data Structures/Linked List/linkedlist.py
+++ Data Structures/Linked List/linkedlist.py
@@ -2,4 +2,3 @@
 # Linked lists differ from lists in the way that they store elements in memory. 
 # While lists use a contiguous memory block to store references to their data, 
 # linked lists store references as part of their own elements.
-
