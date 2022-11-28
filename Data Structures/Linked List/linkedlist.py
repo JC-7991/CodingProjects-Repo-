6c3da6008d1bdca50551070aa5627153d80bb860
JC <jctblock@gmail.com>
2022-11-28 14:57:36 -0500

    Checkpoint

--- Data Structures/Linked List/linkedlist.py
+++ Data Structures/Linked List/linkedlist.py
@@ -1,4 +1,9 @@
 # Linked lists are an ordered collection of objects. 
 # Linked lists differ from lists in the way that they store elements in memory. 
 # While lists use a contiguous memory block to store references to their data, 
-# linked lists store references as part of their own elements. 
+# linked lists store references as part of their own elements.
+
+class LinkedList:
+
+    def __init__(self, r = None):
+        pass
