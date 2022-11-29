d7f3f8ef64db7730ccbd9d15bec037ceb9cbf073
JC <jctblock@gmail.com>
2022-11-29 11:41:18 -0500

    Checkpoint

--- DailyCoding/Week 5/task34.py
+++ DailyCoding/Week 5/task34.py
@@ -1,5 +1,5 @@
 # Given a string, find the palindrome that
-#  can be made by inserting the fewest number 
+# can be made by inserting the fewest number 
 # of characters as possible anywhere in the word. 
 # If there is more than one palindrome of 
 # minimum length that can be made, return 
