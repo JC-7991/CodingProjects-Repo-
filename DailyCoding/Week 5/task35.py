d85e8d03e7892430736ae23f04472cd587c0119e
JC <jctblock@gmail.com>
2022-11-30 11:48:44 -0500

    Checkpoint

--- DailyCoding/Week 5/task35.py
+++ DailyCoding/Week 5/task35.py
@@ -2,3 +2,39 @@
 # segregate the values of the array so that all the Rs come first,
 # the Gs come second, and the Bs come last. You can only swap elements of the array.
 
+def sorted(chars):
+
+    left, right = 0, len(chars) - 1
+
+    while True:
+
+        while chars[left] == 'R' and left < right:
+            left += 1
+
+        while chars[right] != 'R' and left < right:
+            right -= 1
+
+        if left >= right:
+            break
+
+        chars[left], chars[right] = chars[right], chars[left]
+
+    right = len(chars) - 1
+
+    while True:
+
+        while chars[left] != 'B' and left < right:
+            left += 1
+
+        while chars[right] == 'B' and left < right:
+            right -= 1
+
+        if left >= right:
+            break
+
+        chars[left], chars[right] = chars[right], chars[left]
+
+    return chars
+
+if __name__ == '__main__':
+    print (sorted(['G', 'B', 'R', 'R', 'B', 'R', 'G']))
