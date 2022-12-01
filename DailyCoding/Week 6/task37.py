04ff73dfeda989c272533119735da233e3170519
JC <jctblock@gmail.com>
2022-12-01 16:50:58 -0500

    Checkpoint

--- DailyCoding/Week 6/task37.py
+++ DailyCoding/Week 6/task37.py
@@ -1,3 +1,20 @@
 # The power set of a set is the set of all its subsets. 
 # Write a function that, given a set, generates its power set.
 
+import math;
+ 
+def powerSet(set, size):
+     
+    pow_size = (int) (math.pow(2, size))
+    counter = 0
+    j = 0
+
+    for counter in range(0, pow_size):
+        for j in range(0, size):
+            if((counter & (1 << j)) > 0):
+                print(set[j], end = "")
+        print("")
+
+if __name__ == "__main__":
+    set = ['a', 'b', 'c']
+    powerSet(set, 3)
