bc56f4d5b2e775182d87c210c9b50d23a02e62a7
JC <jctblock@gmail.com>
2022-12-04 01:28:46 -0500

    Checkpoint

--- DailyCoding/Week 6/task39.py
+++ DailyCoding/Week 6/task39.py
@@ -5,7 +5,7 @@
 # out only the relevant coordinates, i.e. from the top-leftmost 
 # live cell to bottom-rightmost live cell.
 
-def nextGeneration(grid, x, y):
+def nextGen(grid, x, y):
 
     future = [[0 for i in range(y)] for j in range(x)]
 
@@ -13,9 +13,10 @@ def nextGeneration(grid, x, y):
         for m in range(y):
              
             aliveNeighbours = 0
-            for i in range(-1,2):
-                for j in range(-1,2):
-                    if ((l+i>=0 and l+i<x) and (m+j>=0 and m+j<y)):
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+
+                    if((l + i >= 0 and l + i < x) and (m + j >= 0 and m + j < y)):
                         aliveNeighbours += grid[l + i][m + j]
  
             aliveNeighbours -= grid[l][m]
@@ -30,19 +31,22 @@ def nextGeneration(grid, x, y):
                 future[l][m] = 1
  
             else:
-                future[l][m] = grid[l][m]
- 
+                future[l][m] = grid[l][m] 
  
     print("Next Generation")
+
     for i in range(x):
         for j in range(y):
+
             if (future[i][j] == 0):
-                print(".",end="")
+                print(".", end="")
+
             else:
-                print("*",end="")
-        print()
- 
-x, y = 10,10
+                print("*", end="")
+        print()        
+
+
+x, y = 10, 10
 grid = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
     [ 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 ],
     [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 ],
@@ -56,13 +60,17 @@ grid = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
 ]
 
 print("Original Generation")
+
 for i in range(x):
     for j in range(y):
         
         if(grid[i][j] == 0):
-            print(".",end = "")
+            print(".", end = "")
+
         else:
-            print("*",end = "")
+            print("*", end = "")
+
     print()
+
 print()
-nextGeneration(grid, x, y)
+nextGen(grid, x, y)
