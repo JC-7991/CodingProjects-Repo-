6ee9996173b3ef4504340b7bcb1c55bedc1078e5
JC <jctblock@gmail.com>
2022-12-04 01:23:56 -0500

    Checkpoint

--- DailyCoding/Week 6/task39.py
+++ DailyCoding/Week 6/task39.py
@@ -1,1 +1,6 @@
-# 
+# Implement Conway's Game of Life. It should be able to be initialized 
+# with a starting list of live cell coordinates and the number of 
+# steps it should run for. Once initialized, it should print out 
+# the board state at each step. Since it's an infinite board, print 
+# out only the relevant coordinates, i.e. from the top-leftmost 
+# live cell to bottom-rightmost live cell.
