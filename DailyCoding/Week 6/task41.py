a5672035d9aaf67daa2bc3b8add07fc748361af9
JC <jctblock@gmail.com>
2022-12-05 11:48:04 -0500

    Checkpoint

--- DailyCoding/Week 6/task41.py
+++ DailyCoding/Week 6/task41.py
@@ -5,3 +5,29 @@
 # If there are multiple possible itineraries, return the 
 # lexicographically smallest one. All flights must be used in the itinerary.
 
+def get_itinerary(flight, str):
+
+    itinerary = []
+    src = str
+
+    while flight:
+
+        itinerary.append(src)
+        destinations = [dest for (src, dest) in flight if src == src]
+
+        if not destinations:
+            break
+
+        min_dest = min(destinations)
+        flight.remove((src, min_dest))
+        src = min_dest
+
+    itinerary.append(src)
+
+    if not flight:
+        return itinerary
+
+    return None
+
+if __name__ == "__main__":
+    get_itinerary([('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')], 'YUL') == ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD']
