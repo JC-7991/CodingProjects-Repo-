784310e6817601830f07d78999bafd26da8c92cf
JC <jctblock@gmail.com>
2022-12-04 22:53:37 -0500

    Checkpoint

--- DailyCoding/Week 6/task40.py
+++ DailyCoding/Week 6/task40.py
@@ -1,3 +1,28 @@
 # Given an array of integers where every integer
 # occurs three times except for one integer, which 
 # only occurs once, find and return the non-duplicated integer.
+
+def getSingle(arr, x):
+
+    ones = 0
+    twos = 0
+     
+    for i in range(x):
+
+        twos = twos ^ (ones & arr[i])
+
+        ones = ones ^ arr[i]
+
+        mask = ~(ones & twos)
+
+        ones &= mask
+
+        twos &= mask
+
+    return ones
+
+if __name__ == "__main__":
+
+    arr = [3, 3, 2, 3, 5, 5, 6, 6, 7, 7]
+    x = len(arr)
+    print("The element with single occurrence is", getSingle(arr, x))
