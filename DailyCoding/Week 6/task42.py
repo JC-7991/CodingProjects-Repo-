f8ef559adabf310aa718e6fbc62da1d0cc5070cd
JC <jctblock@gmail.com>
2022-12-06 11:52:53 -0500

    Checkpoint

--- DailyCoding/Week 6/task42.py
+++ DailyCoding/Week 6/task42.py
@@ -2,3 +2,27 @@
 # write a function that returns a subset of S that 
 # adds up to k. If such a subset cannot be made, then return null.
 
+def is_subset(set, x, sum):
+
+    if(sum == 0):
+        return True
+
+    if(x == 0):
+        return False
+  
+    if(set[x - 1] > sum):
+        return is_subset(set, x - 1, sum)
+  
+    return is_subset(set, x - 1, sum) or is_subset(set, x - 1, sum - set[x - 1])
+  
+if __name__  == "__main__":
+
+    set = [3, 34, 7, 10, 15]
+    sum = 9
+    x = len(set)
+
+    if(is_subset(set, x, sum) == True):
+        print("Found a subset with given sum.")
+
+    else:
+        print("No subset with given sum.")
