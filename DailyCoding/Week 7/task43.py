d09d19032d5a0103b5679b1857219bf702966857
JC <jctblock@gmail.com>
2022-12-07 11:57:31 -0500

    Checkpoint

--- DailyCoding/Week 7/task43.py
+++ DailyCoding/Week 7/task43.py
@@ -6,3 +6,37 @@
 # max(), which returns the maximum value in the stack currently.
 # If there are no elements in the stack, then it should throw an error or return null.
 
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+        self.maxes = []
+        
+    def push(self, val):
+
+        self.stack.append(val)
+
+        if self.maxes:
+            self.maxes.append(max(val, self.maxes[-1]))
+            
+        else:
+            self.maxes.append(val)
+
+    def pop(self):
+
+        if self.maxes:
+            self.maxes.pop()
+
+        return self.stack.pop() if self.stack else None
+    
+    def max(self):
+        return self.maxes[-1]
+
+if __name__ == "__main__":
+
+    stack = Stack()
+
+    for i in range(15):
+        stack.push(i)
+
+    print(stack.max())
