e5405b3d6e946cee25a0aacfd15c93c7c17cd208
JC <jctblock@gmail.com>
2022-12-08 21:56:28 -0500

    Checkpoint

---
+++ DailyCoding/Week 7/task44.py
@@ -0,0 +1,7 @@
+# We can determine how "out of order" an array A is
+# by counting the number of inversions it has. 
+# Two elements A[i] and A[j] form an inversion if A[i] > A[j] 
+# but i < j. That is, a smaller element appears after a larger element.
+# Given an array, count the number of inversions it has. 
+# Do this faster than O(N^2) time.
+
