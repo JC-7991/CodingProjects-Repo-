4554d3ca5fa63da920174015fc37843d0f3392bc
JC <jctblock@gmail.com>
2022-12-08 22:03:29 -0500

    Checkpoint

--- DailyCoding/Week 7/task44.py
+++ DailyCoding/Week 7/task44.py
@@ -5,3 +5,45 @@
 # Given an array, count the number of inversions it has. 
 # Do this faster than O(N^2) time.
 
+def inv_count(arr, n):
+    return get_InvCount(arr)[1]
+
+def get_InvCount(arr):
+
+    if len(arr) <= 1:
+        return arr, 0
+
+    middle = int(len(arr) / 2)
+    left, a = get_InvCount(arr[:middle])
+    right, b = get_InvCount(arr[middle:])
+    result, c = merge_count(left, right)
+
+    return result, (a + b + c)
+
+def merge_count(left, right):
+
+    result = []
+    count = 0
+    i, j = 0,0
+    left_len = len(left)
+
+    while i < left_len and j < len(right):
+
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+
+        else:
+            result.append(right[j])
+            count += left_len - i
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+    return result, count   
+
+if __name__ == "__main__":
+
+    arr = [1, 2, 30, 2, 1] 
+    n = len(arr) 
+    print("Number of inversions:", inv_count(arr, n)) 
