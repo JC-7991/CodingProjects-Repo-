5264be65e681e0d296555e8db89a9d9cfcd97739
JC <jctblock@gmail.com>
2022-12-10 17:47:43 -0500

    Checkpoint

--- DailyCoding/Week 7/task46.py
+++ DailyCoding/Week 7/task46.py
@@ -8,25 +8,26 @@ def substr(str, low, high):
 def longestPalin(str):
 
     n = len(str)
-    maxLength = 1
+    maxLen = 1
     start = 0
      
     for i in range(n):
         for j in range(i, n):
             
             flag = 1
+            
             for k in range(0, ((j - i) // 2) + 1):
                 if(str[i + k] != str[j - k]):
                     flag = 0
 
-            if(flag != 0 and (j - i + 1) > maxLength):
+            if(flag != 0 and (j - i + 1) > maxLen):
                 start = i
-                maxLength = j - i + 1
+                maxLen = j - i + 1
                  
     print("Longest palindrome subString is: ", end = "")
-    substr(str, start, start + maxLength - 1)
+    substr(str, start, start + maxLen - 1)
 
-    return maxLength
+    return maxLen
 
 if __name__ == '__main__':
     str = "gohangasalamiimalasgnahog" 
