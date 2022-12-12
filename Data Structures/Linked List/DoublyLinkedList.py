3a8f66fac39848af48742c26cc9525cbf3858ae4
JC <jctblock@gmail.com>
2022-12-12 14:14:17 -0500

    Checkpoint

--- Data Structures/Linked List/DoublyLinkedList.py
+++ Data Structures/Linked List/DoublyLinkedList.py
@@ -42,11 +42,16 @@ class DoublyLinkedList:
         while this_node is not None:
             
             if this_node.data == d:
+
                 if this_node.prev_node is not None:
 
                     if this_node.next_node is not None:
                         this_node.prev_node.next_node = this_node.next_node
                         this_node.next_node.prev_node = this_node.prev_node
+                        
                     else:
                         this_node.prev_node.next_node = None
                         self.last = this_node.prev_node
+                
+                else:
+                    pass
