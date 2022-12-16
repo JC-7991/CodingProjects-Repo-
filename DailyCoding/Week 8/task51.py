886eabee7a57cac11bfc04a830a7ac744a2b6be4
JC <jctblock@gmail.com>
2022-12-15 21:46:26 -0500

    Checkpoint

--- DailyCoding/Week 8/task51.py
+++ DailyCoding/Week 8/task51.py
@@ -6,9 +6,9 @@
 
 from random import randint
 
-def randomizer (arr, n):
+def randomizer (arr, k):
 
-    for i in range(n - 1, 0, -1):
+    for i in range(k - 1, 0, -1):
         j = randint(0, i + 1)
         arr[i], arr[j] = arr[j], arr[i]
 
@@ -17,5 +17,5 @@ def randomizer (arr, n):
 if __name__ == "__main__":
 
     arr = [1, 2, 3, 4, 5, 6, 7, 8]
-    n = len(arr)
-    print(randomizer(arr, n))
+    k = len(arr)
+    print(randomizer(arr, k))
