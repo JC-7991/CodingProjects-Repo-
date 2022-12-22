5c61ca028410fa88255c4909629cb51f0388d120
JC <jctblock@gmail.com>
2022-12-21 23:03:15 -0500

    Checkpoint

--- DailyCoding/Week 9/task57.py
+++ DailyCoding/Week 9/task57.py
@@ -19,10 +19,6 @@ def solve(s, k):
 
         w = x[i]
 
-        if len(w) > k:
-            print('\n[*] Cannot complete because of this word: "%s"' % w)
-            return
-
         if (len(w) + lent + 1) <= k:
 
             lent += len(w) + 1
@@ -45,6 +41,7 @@ def solve(s, k):
 if __name__ == "__main__":
 
     str = "I don’t know why but I just enjoy doing this. Maybe it’s my way of dealing with stress or something but I just do it about once every week. Generally I’ll carry around a sack and creep around in a sort of crouch-walking position making goblin noises, then I’ll walk around my house and pick up various different “trinkets” and put them in my bag while saying stuff like “I’ll be having that” and laughing maniacally in my goblin voice (“trinkets” can include anything from shit I find on the ground to cutlery or other utensils). The other day I was talking with my neighbours and they mentioned hearing weird noises like what I wrote about and I was just internally screaming the entire conversation. I am 99 percent sure they don't know it's me but god that 1 percent chance is seriously weighing on my mind."
+    
     X = solve(str, k = 80)
     for s in X:
         print(s)
