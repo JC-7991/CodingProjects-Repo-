2367bec302e79050596bf2a32d8edb1fe72eb1e6
JC <jctblock@gmail.com>
2022-12-22 11:45:47 -0500

    Checkpoint

--- DailyCoding/Week 9/task58.py
+++ DailyCoding/Week 9/task58.py
@@ -2,3 +2,59 @@
 # Given such an array, find the index of the element in the array 
 # in faster than linear time. If the element doesn't exist in the array, return null.
 
+def pivotSearch(arr, n, key):
+ 
+    pivot = pivotFind(arr, 0, n-1)
+ 
+    if pivot == -1:
+        return binarySearch(arr, 0, n-1, key)
+ 
+    if arr[pivot] == key:
+        return pivot
+
+    if arr[0] <= key:
+        return binarySearch(arr, 0, pivot - 1, key)
+
+    return binarySearch(arr, pivot + 1, n - 1, key)
+
+def pivotFind(arr, low, high):
+ 
+    if high < low:
+        return -1
+
+    if high == low:
+        return low
+
+    mid = int((low + high)/2)
+ 
+    if mid < high and arr[mid] > arr[mid + 1]:
+        return mid
+
+    if mid > low and arr[mid] < arr[mid - 1]:
+        return (mid - 1)
+
+    if arr[low] >= arr[mid]:
+        return pivotFind(arr, low, mid - 1)
+
+    return pivotFind(arr, mid + 1, high)
+
+def binarySearch(arr, low, high, key):
+ 
+    if high < low:
+        return -1
+
+    mid = int((low + high) / 2)
+ 
+    if key == arr[mid]:
+        return mid
+
+    if key > arr[mid]:
+        return binarySearch(arr, (mid + 1), high, key)
+
+    return binarySearch(arr, low, (mid - 1), key)
+ 
+if __name__ == "__main__":
+
+    arr = [5, 6, 7, 8, 9, 10, 1, 2, 3]
+    print("Index of the element is:", \
+        pivotSearch(arr, len(arr), 3))
