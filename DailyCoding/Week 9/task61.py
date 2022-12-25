2657acd4322eb29adc7ba4d91b849abb5e947d14
JC <jctblock@gmail.com>
2022-12-25 12:07:46 -0500

    Checkpoint

--- DailyCoding/Week 9/task61.py
+++ DailyCoding/Week 9/task61.py
@@ -3,3 +3,17 @@
 # Do this faster than the naive method of repeated multiplication.
 # For example, pow(2, 10) should return 1024.
 
+def pow(base: int, power: int) -> int:
+
+    if power == 0:
+        return 1
+
+    if power % 2 != 0:
+        return pow((base * base), power // 2) * base
+
+    return pow((base * base), power // 2)
+
+
+if __name__ == "__main__":
+
+    print(pow(2, 10))
