4ed14c82219ea062101bdacb4080bfb817432504
JC <jctblock@gmail.com>
2022-12-26 12:11:30 -0500

    Checkpoint

--- DailyCoding/Week 9/task62.py
+++ DailyCoding/Week 9/task62.py
@@ -8,3 +8,15 @@
 # Down, then right
 # Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
 
+def paths(m, n):
+
+    if(m == 1 or n == 1):
+        return 1
+
+    return paths(m - 1, n) + paths(m, n - 1)
+ 
+if __name__ == "__main__":
+    
+    m = 3
+    n = 3
+    print(paths(m, n))
