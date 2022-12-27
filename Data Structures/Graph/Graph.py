87e0f7490e1be881cea038543fa6ad7fef172344
JC <jctblock@gmail.com>
2022-12-27 15:23:51 -0500

    Checkpoint

--- Data Structures/Graph/Graph.py
+++ Data Structures/Graph/Graph.py
@@ -1,11 +1,4 @@
-class Vertex:
-
-    def __init__(self, n):
-        self.name = n
-        self.neighbors = set()
-    
-    def add_neighbor(self, v):
-        self.neighbors.add(v)
+from Vertex import *
 
 class Graph:
 
@@ -16,9 +9,12 @@ class Graph:
         if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
             self.vertices[vertex.name] = vertex
             return True
-            
+
         else:
             return False
 
     def add_edge(self, u, v):
-        pass
+        
+        if u in self.vertices and v in self.vertices:
+            self.vertices[u].add_neighbor(v)
+            self.vertices[v].add_neighbor(u)
