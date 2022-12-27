456c8741c837b8c765e85dd33ddf2f2113f370d0
JC <jctblock@gmail.com>
2022-12-27 13:33:56 -0500

    Checkpoint

--- Data Structures/Binary Search Tree/BinarySearchTree.py
+++ Data Structures/Binary Search Tree/BinarySearchTree.py
@@ -88,4 +88,18 @@ class Tree:
                 self.right.inorder()
 
 if __name__ == "__main__":
-    pass
+    
+    tree = Tree(7)
+    tree.insert(9)
+
+    for i in [15, 10, 2, 12, 3, 1, 13, 6, 11, 4, 14, 9]:
+        tree.insert(i)
+
+    for i in range(16):
+        print(tree.find(i), end = ' ')
+        
+    print('\n', tree.get_size())
+    tree.preorder()
+    print()
+    tree.inorder()
+    print()
