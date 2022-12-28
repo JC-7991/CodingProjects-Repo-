23f3b96a6ee17808319fd0e9fbe7c3c7c3e5e2f5
JC <jctblock@gmail.com>
2022-12-27 20:31:57 -0500

    Checkpoint

--- Data Structures/Graph/AdjacencyMatrix.py
+++ Data Structures/Graph/AdjacencyMatrix.py
@@ -14,6 +14,9 @@ class Graph:
         if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
 
             self.vertices[vertex.name] = vertex
-            
+
             for row in self.edges:
                 row.append(0)
+            
+            self.edges.append([0] * (len(self.edges) + 1))
+            self.edge_indices[vertex.name] = len(self.edge_indices)
