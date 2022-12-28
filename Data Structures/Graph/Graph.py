dbfd6f8f977ff0d3f98d4815f4a8c1ce1da327ee
JC <jctblock@gmail.com>
2022-12-27 21:08:19 -0500

    Checkpoint

--- Data Structures/Graph/Graph.py
+++ Data Structures/Graph/Graph.py
@@ -1,4 +1,11 @@
-from Vertex import *
+class Vertex:
+
+    def __init__(self, n):
+        self.name = n
+        self.neighbors = set()
+    
+    def add_neighbor(self, v):
+        self.neighbors.add(v)
 
 class Graph:
 
--- Data Structures/Graph/Vertex.py
+++
@@ -1,8 +0,0 @@
-class Vertex:
-
-    def __init__(self, n):
-        self.name = n
-        self.neighbors = set()
-    
-    def add_neighbor(self, v):
-        self.neighbors.add(v)
