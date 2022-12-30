8d2a7a3abf0504a727fb1d7699cfe9dd40d02e5a
JC <jctblock@gmail.com>
2022-12-29 21:02:43 -0500

    Checkpoint

--- DailyCoding/Weeks 10 - 15/Week 10/task65.py
+++ DailyCoding/Weeks 10 - 15/Week 10/task65.py
@@ -3,10 +3,6 @@
 def spiral(matrix):
 
     ans = []
- 
-    if(len(matrix) == 0):
-        return ans
- 
     m = len(matrix)
     n = len(matrix[0])
 
@@ -17,23 +13,23 @@ def spiral(matrix):
 
     x = 0
     y = 0
-    di = 0
+    z = 0
  
     for i in range(m * n):
 
         ans.append(matrix[x][y])
         seen[x][y] = True
-        cr = x + dr[di]
-        cc = y + dc[di]
+        cr = x + dr[z]
+        cc = y + dc[z]
  
         if (0 <= cr and cr < m and 0 <= cc and cc < n and not(seen[cr][cc])):
             x = cr
             y = cc
 
         else:
-            di = (di + 1) % 4
-            x += dr[di]
-            y += dc[di]
+            z = (z + 1) % 4
+            x += dr[z]
+            y += dc[z]
 
     return ans
 
