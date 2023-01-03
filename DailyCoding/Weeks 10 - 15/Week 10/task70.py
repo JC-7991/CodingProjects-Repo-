3e2d192d57279416c5b9ad1df936301862b8e821
JC <jctblock@gmail.com>
2023-01-03 12:24:48 -0500

    Checkpoint

--- DailyCoding/Weeks 10 - 15/Week 10/task70.py
+++ DailyCoding/Weeks 10 - 15/Week 10/task70.py
@@ -2,3 +2,30 @@
 # Given a positive integer n, return the n-th perfect number.
 # For example, given 1, you should return 19. Given 2, you should return 28.
 
+import itertools
+
+def findNth(n):
+ 
+    count = 0
+ 
+    for cnt in itertools.count():
+
+        sum = 0
+        x = cnt
+
+        while(x):
+            sum = sum + x % 10
+            x = x // 10
+
+        if(sum == 10):
+            count = count + 1
+
+        if (count == n):
+            return cnt
+ 
+    return -1
+
+if __name__== "__main__":
+
+    print(findNth(1))
+    print(findNth(2))
